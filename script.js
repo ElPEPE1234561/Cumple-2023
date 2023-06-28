6f1fc03396280e18cc254a3cf1c26fd7f25b3157
ElPEPE1234561 <87459751+ElPEPE1234561@users.noreply.github.com>
2023-06-27 23:41:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var image = document.getElementById("my-image");
+var alternateImage = document.getElementById("alternate-image");
+var rotateButton = document.getElementById("rotate-button");
+var textFields = document.getElementById("text-fields");
+var downloadButton = document.getElementById("download-button");
+var firstNameInput = document.getElementById("first-name-input");
+
+function showTextFields() {
+  rotateButton.style.display = "none";
+  textFields.style.display = "block";
+}
+
+function rotateImage() {
+  image.style.display = "none";
+  alternateImage.style.display = "block";
+  showTextFields();
+}
+
+function addText() {
+  var firstName = document.getElementById("first-name-input").value;
+  var lastName = document.getElementById("last-name-input").value;
+  var textOverlay = document.getElementById("text-overlay");
+  textOverlay.textContent = firstName + " " + lastName;
+  showDownloadButton();
+}
+
+function showDownloadButton() {
+  downloadButton.style.display = "inline-block";
+  downloadButton.style.position = "static";
+}
+
+function checkDownloadButton() {
+  var nameValue = firstNameInput.value.toLowerCase();
+  if (nameValue === "descargar") {
+    showDownloadButton();
+  } else {
+    downloadButton.style.display = "none";
+  }
+}
+
+function downloadImage() {
+  var imageUrl = document.getElementById("alternate-image").src;
+  var link = document.createElement("a");
+  link.href = imageUrl;
+  link.download = "yo.jpg";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
